46ced76392c6c148f03e3dfc17348de47d8b5dc5
Xu Dong <dsqx71@gmail.com>
2015-09-24 11:16:05 +0800

    add function

--- exp_feature.py
+++ exp_feature.py
@@ -2,70 +2,17 @@
 import pandas as pd
 import numpy as np
 from collections import deque
-from numba import jit,int16
-def find_latest(basic_train,basic_test):
-
-    train  = basic_train[['uid','pid','time']].copy()
-    test   =   basic_test[['uid','pid','time']].copy()
-    tot = pd.concat([train,test],axis=0)
-    tot.sort(columns=['uid','time'],inplace=True)
-    tot['latest']  = np.zeros(tot.shape[0])
-    tot.index = range(tot.shape[0])
-
-    mask = tot['uid'] != tot['uid'].shift(-1)
-    tot['latest'] = tot['time'].diff().shift(-1).dt.days
-    tot[mask,'latest']  = 500
-    train = train.merge(tot[['pid','latest']],left_on='pid',right_on='pid',how='left')
-    test   = test.merge(tot[['pid','latest']],left_on='pid',right_on='pid',how='left')
-
-    train.drop(['uid','time'],axis=1,inplace=True)
-    test.drop(['uid','time'],axis=1,inplace=True)
-    train.set_index('pid',inplace=True)
-    test.set_index('pid',inplace=True)
-    return train,test
-
-def find_seven_days(basic_train,basic_test):
-    '''
-            用队列来维护
-    '''
-    train  = basic_train[['uid','pid','time']].copy()
-    test   = basic_test[['uid','pid','time']].copy()
-    tot = pd.concat([train,test],axis=0)
-    tot.sort(columns=['uid','time'],inplace=True)
-    tot['seven_days']  = np.zeros(tot.shape[0])
-    tot.index = range(tot.shape[0])
-
-    queue =deque([tot.at[tot.shape[0]-1,'time']])
-
-    for x in xrange(tot.shape[0]-2,-1,-1):
-        if tot.at[x,'uid'] != tot.at[x+1,'uid']:
-            queue = deque([])
-        if  len(queue)>0:
-            while  (queue[0]-tot.at[x,'time']).days >7:
-                queue.popleft()
-                if len(queue)==0:
-                    break
-            tot.at[x,'seven_days'] = len(queue)
-        queue.append(tot.at[x,'time'])
-
-    train = train.merge(tot[['pid','seven_days']],left_on='pid',right_on='pid',how='left')
-    test   = test.merge(tot[['pid','seven_days']],left_on='pid',right_on='pid',how='left')
-
-    train.drop(['uid','time'],axis=1,inplace=True)
-    test.drop(['uid','time'],axis=1,inplace=True)
-    train.set_index('pid',inplace=True)
-    test.set_index('pid',inplace=True)
-    return train,test
+from numba import jit,int32,guvectorize
 
 def sentiment_feature(basic_train,basic_test):
 
-    @jit
+    @guvectorize([(int64[:] )], '(n),()->(n)')
     def compute_scores(x):
-        if x['极性'] =='0' or x['极性'] =='3'
+        if x['极性'] =='0' or x['极性'] =='3':
             return 0
         flag =(1 if x['极性']=='1' else -1)
         return  flag * int(x['强度'])
-
+    @vectorize([int32()])
     def compute_sentiment_scores(line):
         words = np.array(line.split(' '))
         mask = sentiment['词语'].isin(words)
--- learning.py
+++ learning.py
@@ -1,5 +1,4 @@
 __author__ = 'DongXu'
-
 import validation
 import numpy as np
 import pandas as pd
@@ -14,9 +13,9 @@ if __name__ == '__main__':
 
     logging.info('Loading data...')
 
-    train_x = np.load('processed_data/train_np.npy')
-    test_x  = np.load('processed_data/test_np.npy')
-    train_y = np.load('processed_data/target_np.npy')
+    train_x = np.load('processed_data/train3_np.npy')
+    test_x  = np.load('processed_data/test3_np.npy')
+    train_y = np.load('processed_data/target3_np.npy')
 
     train_y_class = np.zeros_like(train_y)
     train_y_class[train_y[:,0]>1,0] = 1
@@ -24,24 +23,21 @@ if __name__ == '__main__':
     train_y_class[train_y[:,2]>0,2] = 1
     train_y = np.log1p(train_y)
 
-    train_basic = pd.read_pickle('raw_data/basic_train')
-#    score = validation.make_scorer(validation.my_score_func,greater_is_better=True)
-
     logging.info('Start to classify')
-    clf = RandomForestClassifier(n_estimators=2000,max_features=25,max_leaf_nodes=5000,n_jobs=3,verbose=2)
+    clf = RandomForestClassifier(n_estimators=150,max_features=25,max_leaf_nodes=2000,n_jobs=1,verbose=2)
     clf.fit(train_x,train_y_class)
     test_predict = clf.predict(test_x)
 
+    np.save('2015-9-24',test_predict)
     logging.info('Start to regression')
     for i in xrange(3):
         mask = train_y_class[:,i] >0
-        regressor = RandomForestRegressor(n_estimators=3000,max_features=50,max_leaf_nodes=5000,n_jobs=3,verbose=2)
+        regressor = RandomForestRegressor(n_estimators=150,max_features=75,max_leaf_nodes=2000,n_jobs=1,verbose=2)
         regressor.fit(train_x[mask],train_y[mask,i])
         test_predict[test_predict[:,i]>0,i] = np.rint(np.expm1(regressor.predict(test_x[test_predict[:,i]>0])))
 
     test_predict[test_predict<0] = 0
     logging.info('Start to output result')
-    np.save('2015-9-19-np',test_predict)
-    access_data.output_result(test_predict,'2015-9-20')
+    access_data.output_result(test_predict,'2015-9-24')
     logging.info('Finished ! ')
 
